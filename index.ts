ba1436eb7f0d06a6e222d7b2d2c0b6a2cd6703c1
jinwoo6531 <jintakk6531@gmail.com>
2022-02-07 09:07:33 +0900

    2/7

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
+
+//json 데이터 받아오기
 axios .get(url).then((response) => {
     console.log(response.data)
 })
